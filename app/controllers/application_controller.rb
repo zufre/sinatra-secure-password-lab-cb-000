8656693812672072ee522f71a09966d2809cb7f4
Cedric David <cedric.david@btopenworld.com>
2018-09-23 03:15:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,67 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-      redirect '/login'
+    @user = User.new(:username => params[:username], :password => params[:password])
+    if @user.save
+      redirect to '/login'
+    else
+      redirect to '/failure'
+    end
 
+  end
+
+  get '/account' do
+    if current_user
+      erb :account
+    else
+      erb :failure
+    end
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/account'
     else
       redirect '/failure'
     end
   end
+
+  get "/success" do
+    if logged_in?
+      erb :account
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+end
+
 
   get '/account' do
     @user = User.find(session[:user_id])
